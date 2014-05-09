e4c326b484ad57a3b8fcdee17780de325c0fa169
atachimiko <atachi.miko@gmail.com>
2014-05-09 13:26:21 +0900

    Initialize Repository

--- .NET4.5/Atlib.Common/Guard.cs
+++ .NET4.5/Atlib.Common/Guard.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Atlib.Common
+namespace Atlib
 {
 	/// <summary>
 	/// Contains common methods that guard parameter inputs.
